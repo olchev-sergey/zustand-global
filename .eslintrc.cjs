394188108f89cfc4fea674f94752fb8f3bcd0a88
Olchev Sergey <sergey200012111sergey@gmail.com>
2024-01-27 14:29:09 +0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended', // must be last
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['prettier', '@typescript-eslint', 'react'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': ['error', 'ignorePackages'],
+
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+  },
+}
